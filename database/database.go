c67f5d38314772feeb774bfeaf4d808e07b4a74d
Adriano Monteiro <47261292+adrianomonteiroweb@users.noreply.github.com>
2024-06-16 11:55:40 -0300

    new: database connection with sqlite

---
+++ database/database.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	"github.com/adrianomonteiroweb/library-golang-api/models"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConectarBancoDeDados() {
+    database, err := gorm.Open(sqlite.Open("libraryGolang.db"), &gorm.Config{})
+    if err != nil {
+        panic("Error trying to connect to database: " + err.Error())
+    }
+
+    database.AutoMigrate(&models.Book{}, &models.Author{})
+
+    DB = database
+}
